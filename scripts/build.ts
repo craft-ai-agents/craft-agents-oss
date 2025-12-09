40f51358d6e4fbd80871dc5e2daeb7453a8792c4
Levy Melnikov <levy@craft.do>
2025-12-09 14:37:55 +0100

    Fix build time vars

--- scripts/build.ts
+++ scripts/build.ts
@@ -59,7 +59,7 @@ for (const target of TARGETS) {
       --target=${target.bunTarget} \
       --external yoga-wasm-web \
       --external keytar \
-      --define BUILD_VERSION="${VERSION}" \
+      --define VERSION="${VERSION}" \
       --define BUILD_TIME="${BUILD_TIME}" \
       --define BUILD_TIMESTAMP="${BUILD_TIMESTAMP}" \
       src/index.tsx \
